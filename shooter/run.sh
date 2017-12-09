ba4f748d172a09de3e5521f5a57c739d91b53897
lidicn <30893504+lidicn@users.noreply.github.com>
2017-12-09 15:42:01 +0800

    test beta1

--- shooter/run.sh
+++ shooter/run.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-
-python3 /usr/src/app/subfinder.py
+#!/bin/bash
+set -e
+options=$(cat /data/options.json | jq -r 'if .options then [.options[] | "-o "+.name+"="+.value ] | join(" ") else "" end')
+python3 /usr/src/app/SubFinder.py -r /transmission/downloads/complete
